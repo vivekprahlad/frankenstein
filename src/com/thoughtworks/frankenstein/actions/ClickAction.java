a371c101e6df31c956bf4033a8533dcd5da1cfa1
Vivek Prahlad <vivek.prahlad@gmail.com>
2006-08-24 04:20:15 +0000

    Updating click action to notify completion either when a window is shown or when the mouse is released

--- src/com/thoughtworks/frankenstein/actions/ClickAction.java
+++ src/com/thoughtworks/frankenstein/actions/ClickAction.java
@@ -1,18 +1,17 @@
 package com.thoughtworks.frankenstein.actions;
 
 import java.awt.*;
-import java.awt.event.InputEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 
 /**
  * Understands clicking on a component.
  */
-public class ClickAction extends AbstractMouseAction implements Action, MouseListener {
+public class ClickAction extends AbstractMouseAction implements Action, MouseListener, AWTEventListener {
 
     public synchronized void execute(Component component, Robot robot) {
         moveMouse(component, robot);
         component.addMouseListener(this);
+        Toolkit.getDefaultToolkit().addAWTEventListener(this, AWTEvent.WINDOW_EVENT_MASK);
         click(robot);
         try {
             wait();
@@ -20,21 +19,28 @@ public class ClickAction extends AbstractMouseAction implements Action, MouseLis
             throw new RuntimeException(e);
         }
         component.removeMouseListener(this);
+        Toolkit.getDefaultToolkit().removeAWTEventListener(this);
     }
 
-    public synchronized void mouseClicked(MouseEvent e) {
+    public void mouseClicked(MouseEvent e) {
     }
 
     public synchronized void mousePressed(MouseEvent e) {
-        notifyAll();
     }
 
-    public void mouseReleased(MouseEvent e) {
+    public synchronized void mouseReleased(MouseEvent e) {
+        notifyAll();
     }
 
     public void mouseEntered(MouseEvent e) {
     }
 
     public void mouseExited(MouseEvent e) {
+    }
+
+    public synchronized void eventDispatched(AWTEvent event) {
+        if (event.getID() == WindowEvent.WINDOW_OPENED) {
+            notifyAll();
+        }
     }
 }
