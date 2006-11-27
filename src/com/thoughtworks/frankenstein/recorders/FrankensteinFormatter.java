42434a7e033c0bd528b17e643009ff241995ea44
Vivek Prahlad <vivek.prahlad@gmail.com>
2006-11-27 11:04:20 +0000

    Fixing formatter test

--- src/com/thoughtworks/frankenstein/recorders/FrankensteinFormatter.java
+++ src/com/thoughtworks/frankenstein/recorders/FrankensteinFormatter.java
@@ -1,8 +1,10 @@
 package com.thoughtworks.frankenstein.recorders;
 
+import javax.swing.text.DateFormatter;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 /**
  * Understands SOMETHING
@@ -13,8 +15,9 @@ public class FrankensteinFormatter extends Formatter {
     public String format(LogRecord record) {
         StringBuffer sb = new StringBuffer();
         Date date = new Date(record.getMillis());
+        DateFormatter formatter = new DateFormatter();
         sb.append(record.getLevel() + ": ");
-        sb.append(date.toString() + " ");
+        sb.append(new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a").format(date) + " ");
         if (record.getThrown()!=null) {
             sb.append("Exception: " + record.getThrown().getMessage() + "\n");
             logException(sb, record.getThrown());
--- test/com/thoughtworks/frankenstein/recorders/FrankensteinFormatterTest.java
+++ test/com/thoughtworks/frankenstein/recorders/FrankensteinFormatterTest.java
@@ -14,14 +14,14 @@ public class FrankensteinFormatterTest extends TestCase {
     public void testFormatsMessagesWithLogLevelDateAndMessage() {
         LogRecord record = new LogRecord(Level.INFO, "Message");
         record.setMillis(1000);
-        assertEquals("INFO: Thu Jan 01 00:00:01 GMT 1970 Message\n", new FrankensteinFormatter().format(record));
+        assertEquals("INFO: 01/01/1970 12:00:01 AM Message\n", new FrankensteinFormatter().format(record));
     }
 
     public void testFormatsExceptionMessagesWithLogLevelDateAndMessage() {
         LogRecord record = new LogRecord(Level.INFO, "Message");
         record.setMillis(1000);
         record.setThrown(createException());
-        assertEquals("INFO: Thu Jan 01 00:00:01 GMT 1970 Exception: Foo\n" +
+        assertEquals("INFO: 01/01/1970 12:00:01 AM Exception: Foo\n" +
                 "\t\t\t\tcom.test.class.someMethod(Test.java:10)\n" +
                 "Message\n", new FrankensteinFormatter().format(record));
     }
