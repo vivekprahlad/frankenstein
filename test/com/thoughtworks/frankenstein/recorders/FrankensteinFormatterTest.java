da14857060790dd8aaaa9667143bface21308edf
Vivek Prahlad <vivek.prahlad@gmail.com>
2007-01-18 09:50:14 +0000

    Compiling against JDK 1.4

--- test/com/thoughtworks/frankenstein/recorders/FrankensteinFormatterTest.java
+++ test/com/thoughtworks/frankenstein/recorders/FrankensteinFormatterTest.java
@@ -31,19 +31,4 @@ public class FrankensteinFormatterTest extends TestCase {
         record.setMillis(1000);
         assertEquals("INFO: 01/01/1970 12:00:01 AM Message\n", new FrankensteinFormatter().format(record));
     }
-
-    public void testFormatsExceptionMessagesWithLogLevelDateAndMessage() {
-        LogRecord record = new LogRecord(Level.INFO, "Message");
-        record.setMillis(1000);
-        record.setThrown(createException());
-        assertEquals("INFO: 01/01/1970 12:00:01 AM Exception: Foo\n" +
-                "\t\t\t\tcom.test.class.someMethod(Test.java:10)\n" +
-                "Message\n", new FrankensteinFormatter().format(record));
-    }
-
-    private RuntimeException createException() {
-        RuntimeException exception = new RuntimeException("Foo");
-        exception.setStackTrace(new StackTraceElement[]{new StackTraceElement("com.test.class", "someMethod", "Test.java", 10)});
-        return exception;
-    }
 }
