acd1a54b8efc71cb00cec4371835c83c15c991fc
Pavan KS <ps@thoughtworks.com>
2007-02-09 09:31:24 +0000

    Testing openqa access

--- src/com/thoughtworks/frankenstein/application/Application.java
+++ src/com/thoughtworks/frankenstein/application/Application.java
@@ -4,5 +4,7 @@ package com.thoughtworks.frankenstein.application;
  * Implement this interface to start an application
  */
 public interface Application {
+
     public void launch(String[] args);
+
 }
