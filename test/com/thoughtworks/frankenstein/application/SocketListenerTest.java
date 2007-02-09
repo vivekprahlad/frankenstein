f0aaa71364cedad47f6291156ef09f4da352c3fa
Prakash Subramaniam <psubram1@thoughtworks.com>
2007-02-09 09:46:20 +0000

    Dummy checkin to configure cruise control.

--- test/com/thoughtworks/frankenstein/application/SocketListenerTest.java
+++ test/com/thoughtworks/frankenstein/application/SocketListenerTest.java
@@ -24,6 +24,7 @@ public class SocketListenerTest extends MockObjectTestCase {
         SocketListener listener = new SocketListener(recorder);
         listener.start(Constants.LISTEN_PORT);
         Socket socket = new Socket(InetAddress.getLocalHost(), Constants.LISTEN_PORT);
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         writer.write("EnterText \"textField\" \"abc\"\n");
